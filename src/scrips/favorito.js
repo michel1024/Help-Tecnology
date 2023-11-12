a25afbed127ec7491e3a392adaf7fa9ab202ae6d
Erick  Garcia <egarcia@sistemas-expertos.com>
2023-11-12 15:56:59 -0500

    marcar y desmarcar productos favoritos, botón de WhatsApp

--- src/scrips/favorito.js
+++ src/scrips/favorito.js
@@ -1,3 +1,4 @@
+let btnStar = document.getElementById("star");
 const favoritosContent = document.getElementById("favoritos-content");
 let favoritosList = [];
 
@@ -9,7 +10,7 @@ for(let i = 0; i < btnFavoritos.length; i++){
     let el = btnFavoritos[i]
     let prodEl = el.parentElement.parentElement.parentElement;
     el.addEventListener("click", () => {
-        saveFavorito(prodEl);
+        saveFavorito(prodEl, el);
     })
 }
 
@@ -26,10 +27,18 @@ const initFavoritos = () => {
             favoritosTemplate += `
                 ${item ? item.element : ""}
             `;
+
+            let btnF = document.getElementById(`${item.id}`);
+            btnF.classList.add("active");
         }
     
         favoritosContent.innerHTML = favoritosTemplate;
+        btnStar.innerHTML = `<span>${favoritosList.length}</span>`;
     }else{
+        let btnsF = document.getElementsByClassName("btn-favorito");
+        for(let i = 0; i < btnsF.length; i++){
+            btnsF[i].classList.remove("active");
+        }
         btnVaciar.classList.add("hidden");
         // favoritosContent.innerHTML = "<span class='no-favoritos'>No tienes favoritos <i class='fa-regular fa-face-frown-open'></i></span>";
         favoritosContent.innerHTML = "<span class='no-favoritos'><i class='fa-solid fa-star'></i></span>";
@@ -37,9 +46,10 @@ const initFavoritos = () => {
     }
 }
 
-const saveFavorito = (element) => {
+const saveFavorito = (element, el) => {
+    console.log({el});
     console.log(element.outerHTML);
-    favoritosList.push({element: element.outerHTML})
+    favoritosList.push({element: element.outerHTML, id: `${el.id}`})
     localStorage.setItem('favoritos', JSON.stringify(favoritosList))
     initFavoritos();
 }
@@ -51,6 +61,7 @@ btnVaciar.addEventListener("click", () => {
         carrProd[i].remove();
     }
     favoritosList = [];
+    btnStar.innerHTML = "";
     initFavoritos();
 })
 
